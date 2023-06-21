c74063b71f94728c2732d084339cbf485715f363
robinzimmermann <robin.zimmermann@gmail.com>
2023-06-20 18:12:11 -0700

    Remove delete search handler

--- src/controllers/v3/controller.ts
+++ src/controllers/v3/controller.ts
@@ -1,23 +1,14 @@
 import { Request, Response } from 'express';
 
 import { publicDir } from '../../globals';
-import { RES_SEARCH, RES_SEARCHES, RES_SUCCESS, RES_USER_PREFS } from './models';
+import { RES_SEARCH, RES_SEARCHES, RES_USER_PREFS, RES } from './models';
 import * as dbSearches from '../../database/dbSearches';
 import * as dbUserPrefs from '../../database/dbUserPrefs';
-import * as fetcher from '../../fetcher/fetcher';
 import { removeAnsiCodes } from '../../utils/utils';
+import { logger } from '../../utils/logger/logger';
 
 export const rootHandler = (_req: Request, res: Response): void => {
-  // res.status(200).send('v3 root');xxsxzz
-  // res.sendFile(
-  //   '/Users/rozimmermann/doc/2021.10.18_Craigslist-scraper/express-api-starter-ts/public/v3.html',
-  // );
   res.sendFile(`${publicDir}/v3.html`);
-};
-
-export const isAliveHandler = (_req: Request, res: Response): void => {
-  const response: RES_SUCCESS = { success: true };
-  res.status(200).json(response);
 };
 
 export const getSearchesHandler = (_req: Request, res: Response): void => {
@@ -41,26 +32,29 @@ export const upsertSearchHandler = (req: Request, res: Response): void => {
   }
 };
 
+// export const deleteSearchHandler = (req: Request, res: Response): void => {
+//   logger.silly(`deleting sid ${req.params.sid}`);
+//   // const result = dbSearches.upsert(req.body);
+//   // if (result.isOk()) {
+//   //   const response = <RES_SEARCH>{ success: true, search: result.value };
+//   //   // response.search.alias = response.search.alias + 'X';
+//   //   res.status(200).json(response);
+//   // } else {
+//   //   const response = <RES_SEARCH>{
+//   //     success: false,
+//   //     search: req.body,
+//   //     reason: removeAnsiCodes(result.error),
+//   //   };
+//   //   res.status(400).json(response);
+//   // }
+//   res.send(200).json(<RES>{ success: true });
+// };
+
 export const getUserPrefsHandler = (_req: Request, res: Response): void => {
   const response: RES_USER_PREFS = { userPrefs: dbUserPrefs.getUserPrefs() };
   res.status(200).json(response);
 };
 
-export const startScanHandler = async (_req: Request, res: Response): Promise<void> => {
-  await fetcher.doSearch();
-  const response: RES_SUCCESS = { success: true };
-  res.status(200).json(response);
-};
-
-export const exampleHandler = (_req: Request, res: Response): void => {
-  res.status(200).json({ stuff: 'v3 example request' });
-};
-
 export const deadHandler = (_req: Request, res: Response): void => {
   res.status(400).json({ stuff: 'v3 dead request (' + _req.method + ')' });
 };
-
-// const exampleHandler = (req: Request, res: Response, next: NextFunction) => {
-//     //Throwing some error
-//     next(new HttpException(400,'Bad Request'));
-// };
--- src/routes/v3/routes.ts
+++ src/routes/v3/routes.ts
@@ -6,17 +6,13 @@ const router = Router();
 
 router.get('/', v3Controllers.rootHandler);
 
-router.get('/isAlive', v3Controllers.isAliveHandler);
-
 router.get('/searches', v3Controllers.getSearchesHandler);
 
 router.put('/searches/upsert/:sid', v3Controllers.upsertSearchHandler);
 
+// router.delete('/searches/searchTerm/:sid/:source/:index', v3Controllers.deleteSearchHandler);
+
 router.get('/userPrefs', v3Controllers.getUserPrefsHandler);
-
-router.get('/startScan', v3Controllers.startScanHandler);
-
-router.get('/example', v3Controllers.exampleHandler);
 
 router.use('/*', v3Controllers.deadHandler);
 
